296db4fd7f450ce1a61a2d333c842cb7d9b30084
Christopher Allen-Poole <christopherw@allen-poole.com>
2011-09-08 16:36:39 -0400

    2.2

--- 2.2.c
+++ 2.2.c
@@ -3,7 +3,7 @@
 MAIN{
     int c, i, lim;
     lim = 5;
-    
-    for(i = 0; (i < lim - 1)?((c = getchar()) != '\n'? c != EOF:0):0; i++)
+    // parens... for all of those with a Lisp.
+    for(i = 0; ((i < lim - 1)?((c = getchar()) != '\n'? c != EOF:0):0); i++)
         putchar(c);
 }
